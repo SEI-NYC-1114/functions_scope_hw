fd7c8b38a1d6145686efe55ff5d0d2476eadb35f
Jagerziel <fromnytosg@gmail.com>
2022-11-17 16:52:30 -0500

    Commit 5 - Sum Array

--- app.js
+++ app.js
@@ -50,11 +50,11 @@ function sumArray(arr) {
     sum = 0;
     for (let i = 0; i < arr.length; i++) {
         sum += arr[i];
-    }
+    } 
     return sum;
 }
 
-console.log(sumArray([1, 2, 3, 4, 5, 6]));
+console.log(sumArray([1, 2, 3, 4, 5, 6])); //testing code
 
 // 6.1 checkPrime
 function checkPrime(num) {
