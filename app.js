f2715b80a8fbdd8648350e68e9acca77ad7aa9ff
Nicholas Fasulo <fasulo.nick@gmail.com>
2022-11-17 18:03:49 -0500

    Commit 1 - Verbal questions

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // 1.1. What is the difference between a parameter and an argument?
+// A parameter is what a function takes in as its being written,
+// an argument is what a function takes in as its being invoked.
 
 // 1.2. Within a function, what is the difference between return and console.log?
+// "return" will return the value that follows it, console.log will log the value to the console.
 
 // 1. 3. What are the implications of the ability of a function to return a value?
+// 
 
 // 2. calculateCube
 function calculateCube(num) {
