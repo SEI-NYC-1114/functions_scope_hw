a4d887af8dbf90328ad744cb62c187a54603d018
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2022-11-17 16:03:49 -0500

    Commit 1 - Verbal questions

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // 1.1. What is the difference between a parameter and an argument?
+// A parameter is what goes in the parentheses when you declare a function. i.e. function exampleFunc(para, meter).
+
+// An argument is what you feed a function when you invoke it, and then it becomes the parameter when the function is run. i.e. console.log(exampleFunc().
 
 // 1.2. Within a function, what is the difference between return and console.log?
 
+// Return is like the output of the function. When the function hits a return, it exits. Console.log just prints the JS to the console.
+
 // 1. 3. What are the implications of the ability of a function to return a value?
+// You may use functions to run chunks of code and return a new value that can be stored in a new variable. This is crucial if your function is an intermediate step in a larger problem that you're working out.
 
 // 2. calculateCube
 function calculateCube(num) {
