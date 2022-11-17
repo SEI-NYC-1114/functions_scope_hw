3f04291ef3c46763a002324f78d3b2df26c5428d
Jagerziel <fromnytosg@gmail.com>
2022-11-17 16:25:31 -0500

    Commit 1 - Verbal questions

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // 1.1. What is the difference between a parameter and an argument?
+/*
+Parameter: the argument the function takes - i.e. myFunction(name) {}
+Argument: the variable passed into the function.  i.e. myFunction('Ryan');
 
+*/
 // 1.2. Within a function, what is the difference between return and console.log?
-
+/*
+console.log will return a value to the terminal.  It is helpful for reviewing and returning code.
+return will produce the executed function to be used again.  I.e. sum(3,4) will produce 7 (ideally) and that 7 can be used within other formulas.
+*/
 // 1. 3. What are the implications of the ability of a function to return a value?
+/*
+When the function returns a value it can be used again.  This allows for functions to be created instead of writing the same code over and over again.  In other words, if you were going to add two numbers several times, it's better to create the function and pass the numbers through the function so you don't have to 'recreate the wheel' (bad example as this is overly simple but hopefully makes the point)
+*/
+
+
 
 // 2. calculateCube
 function calculateCube(num) {
