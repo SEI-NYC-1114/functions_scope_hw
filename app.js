b341e0467005bc65a0d274a805635ea0cdeffa2c
kwonangela <kwonangelaa@gmail.com>
2022-11-18 16:39:55 -0500

    Commit 5- Sum Array

--- app.js
+++ app.js
@@ -38,12 +38,19 @@ function getTwoLengths(word1, word2) {
     
     return lenArr;
 }
-console.log(getTwoLengths("Hank", "Hippopopalous")); 
+//console.log(getTwoLengths("Hank", "Hippopopalous")); 
 
 // 5. sumArray
 function sumArray(arr) {
     // YOUR CODE HERE
+    let sum = 0;
+    for (let i=0;i<arr.length;i++){
+        sum = arr[i] + sum;
+    }
+    return sum;
 }
+
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
 // 6.1 checkPrime
 function checkPrime(num) {
