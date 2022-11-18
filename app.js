5a15dcee1d6dce7278f7c7b24ed43b25fc54596b
Edward Olstrom <eddieolstrom1@gmail.com>
2022-11-18 14:48:22 -0500

    Commit 5- Sum array

--- app.js
+++ app.js
@@ -32,15 +32,23 @@ console.log(isAVowel("a"));
 
 // 4. getTwoLengths
 function getTwoLengths(word1, word2) {
-    // YOUR CODE HERE
+    array = [];
+    array.push(word1.length, word2.length);
+    return array
 }
 
 console.log(getTwoLengths("Hank", "Hippopopalous"));
 
 // 5. sumArray
 function sumArray(arr) {
-    // YOUR CODE HERE
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum
 }
+
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
 // 6.1 checkPrime
 function checkPrime(num) {
