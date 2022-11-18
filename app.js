17e530d113257fac7996c7ae852a9ec565e05e54
anna <anna@rcbd.org>
2022-11-17 22:39:20 -0500

    Commit 10 - Longest Word

--- app.js
+++ app.js
@@ -167,7 +167,15 @@ findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]);
 // 10. sumPositive
 function sumPositive(arr) {
   // YOUR CODE HERE
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
 }
+console.log(sumPositive([1, -4, 7, 12]));
 
 module.exports = {
   calculateCube,
