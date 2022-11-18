ae8c4a27b928b68877bbfd6a579289ebabe1262d
Orlando <orlandopichardo809@gmail.com>
2022-11-18 14:56:28 -0500

    Two Lengths

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 // 1.1. What is the difference between a parameter and an argument?
 
+// The parameter is the name that you give the function.  The argument is the value that we pass to the function that we give it.
+
 // 1.2. Within a function, what is the difference between return and console.log?
+
+//console.log tests your code. Return is used for returning values.
 
 // 1. 3. What are the implications of the ability of a function to return a value?
 
+//the values that a function returned when its complete for ex. true or false. 
+
 // 2. calculateCube
 function calculateCube(num) {
-    // YOUR CODE HERE
+    return num * num * num;
 }
+
+console.log(calculateCube(5));
 
 // 3. isAVowel
 function isAVowel(letter) {
-    // YOUR CODE HERE
+  if (letter.toLowerCase() === 'a' || letter.toLowerCase() === 'e' || letter.toLowerCase() === 'i' || letter.toLowerCase() === 'o' || letter.toLowerCase() === 'u') {
+    return true;
+  } else {
+return false;
+  }
 }
+
+console.log(isAVowel("a"));
+
+
 
 // 4. getTwoLengths
 function getTwoLengths(word1, word2) {
-    // YOUR CODE HERE
-}
+    let getTwoLengths = ["Hank", "Hippopopalous"]
+        return [word1.length, word2.length]
+    }
+
+console.log(getTwoLengths("Hank", "Hippopopalous"));
+
 
 // 5. sumArray
 function sumArray(arr) {
@@ -57,14 +77,14 @@ function sumPositive(arr) {
 }
 
 module.exports = {
-    calculateCube,
-    isAVowel,
-    getTwoLengths,
-    sumArray,
-    checkPrime,
-    printPrimes,
-    printLongestWord,
-    eulerFibo,
-    findNeedle,
-    sumPositive
+    // calculateCube,
+    // isAVowel,
+    // getTwoLengths,
+    // sumArray,
+    // checkPrime,
+    // printPrimes,
+    // printLongestWord,
+    // eulerFibo,
+    // findNeedle,
+    // sumPositive
 };
