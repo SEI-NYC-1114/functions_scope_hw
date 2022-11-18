aa9bcbc9c09d60a56655259a34da73365a92bf78
alpakas96 <jhurleyfilm@gmail.com>
2022-11-17 20:09:44 -0500

    Commit 1 - Verbal questions

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // 1.1. What is the difference between a parameter and an argument?
+// a. parameters are declared along with and within the function. 
+// The argument is a value passed to the function after it is declared. 
 
-// 1.2. Within a function, what is the difference between return and console.log?
+// 1.2. Within a function, what is the difference between 
+//return and console.log?
 
-// 1. 3. What are the implications of the ability of a function to return a value?
+//return is the new value of the argument variable.
+//console.log prints the value of the function once the argument is 
+//passed, but doesn't change the value of the argument. 
+
+// 1. 3. What are the implications of the ability
+// of a function to return a value?
+//It allows you to use multiple variables with a function, programatically. 
+//i.e. use the same function with different variable values. 
 
 // 2. calculateCube
 function calculateCube(num) {
