a860e76ee68cc2e90aab2eea373f1dca20004a6f
muhammadwarrad <moe_warrad@yahoo.com>
2022-11-20 13:08:49 -0500

    done

--- app.js
+++ app.js
@@ -5,81 +5,94 @@
 // // 1. 3. What are the implications of the ability of a function to return a value? return ends the function and returns it
 
 // // 2. calculateCube
-// function calculateCube(num) {
-// //     // YOUR CODE HERE
-//     return Math.pow(num, 3); 
-    
-    
-// }
-// console.log(calculateCube(5));
-// // // 3. isAVowel
-// function isAVowel(letter) {
+function calculateCube(num) {
 //     // YOUR CODE HERE
-//     if(letter.toLowerCase()=== "a" || letter.toLowerCase()==="e"){
-//         return true; 
-//     }else if(letter.toLowerCase()==="i"|| letter.toLowerCase()==="o"){ return true;
-//     }else if(letter.toLowerCase()==="u"){ return true;
-// }else {return false}
-// }
-// console.log(isAVowel("a"))
+    return Math.pow(num, 3); 
+    
+    
+}
+console.log(calculateCube(5));
+// // // 3. isAVowel
+function isAVowel(letter) {
+//     // YOUR CODE HERE
+    if(letter.toLowerCase()=== "a" || letter.toLowerCase()==="e"){
+        return true; 
+    }else if(letter.toLowerCase()==="i"|| letter.toLowerCase()==="o"){ return true;
+    }else if(letter.toLowerCase()==="u"){ return true;
+}else {return false}
+}
+console.log(isAVowel("a"))
 
 // // 4. getTwoLengths
-// function getTwoLengths(word1, word2) {
-//     // YOUR CODE HERE
-//     let arr = [];
-//     arr.push(word1.length);
-//     arr.push(word2.length);
-//     return arr;
-// }
-// console.log(getTwoLengths("Hank", "Hippopopalous"));
+function getTwoLengths(word1, word2) {
+    // YOUR CODE HERE
+    let arr = [];
+    arr.push(word1.length);
+    arr.push(word2.length);
+    return arr;
+}
+console.log(getTwoLengths("Hank", "Hippopopalous"));
 // // 5. sumArray
-// function sumArray(arr) {
+function sumArray(arr) {
      // YOUR CODE HERE
-//     let sum = 0
-//     for(let i = 0; i < arr.length; i++){
-//         sum += arr[i];
-//     }
-//     return sum;
-// }
-// console.log(sumArray([1, 2, 3, 4, 5, 6]));
+    let sum = 0
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
 // // 6.1 checkPrime
-//function checkPrime(num) {
+function checkPrime(num) {
 //     // YOUR CODE HERE
-// for (let i = 2; i<= Math.floor(Math.sqrt(num)); i++) {
-//     if (num % i === 0) {
-//         return false;
-//     }
-// }
-//  return true;
-// }
-// console.log(checkPrime(10));
+for (let i = 2; i<= Math.floor(Math.sqrt(num)); i++) {
+    if (num % i === 0) {
+        return false;
+    }
+}
+ return true;
+}
+console.log(checkPrime(10));
 // // // // 6.2 printPrimes
-//  function printPrimes(num) {
-//  for(let i = 2; i <= num; i++) {
-// if (checkPrime(i)=== true){
-//     console.log(i);
-// }
-//  }
+ function printPrimes(num) {
+ for(let i = 2; i <= num; i++) {
+if (checkPrime(i)=== true){
+    console.log(i);
+}
+ }
 
 // YOUR CODE HERE
-// }
-// printPrimes(97)
+}
+printPrimes(97)
 // // 7. printLongestWord
 function printLongestWord(arr) {
 //     // YOUR CODE HERE
-let logestWord = arr.split("").sort(function(a, b) { return b.length - a.length;});
-return LongestWord [0].length;
+let longestWord = arr.sort(function(a, b) { return b.length - a.length;});
+return longestWord[0];
 
  }
-console.log("BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "blob")
+console.log (printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "blob"]));
 // // BONUS!
 
 // // 8. eulerFibo
-// function eulerFibo(num) {
+ function eulerFibo(num) {
 //     // YOUR CODE HERE
-// }
-
+    let fiboc=[1,2]
+    let total= 2
+    while(fiboc[fiboc.length -1] < num) {
+        let nextnum = fiboc[fiboc.length - 1] + fiboc[fiboc.length -2]
+        // console.log(nextnum)
+        fiboc.push(nextnum)
+        if(nextnum % 2 === 0 && nextnum < num) {
+            total += nextnum
+        }
+    }
+    console.log(fiboc)
+    return total
+}
+console.log(eulerFibo(100))
+console.log(eulerFibo(4000000))
 // // 9. findNeedle
 // function findNeedle(arr) {
 //     // YOUR CODE HERE
