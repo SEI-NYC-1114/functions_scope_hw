9743a28b0b6dcf948d3de7164dd9e30f91eb8c61
Orlando <orlandopichardo809@gmail.com>
2022-11-20 22:47:30 -0500

    Orlando Funtions Scope HW

--- app.js
+++ app.js
@@ -54,13 +54,23 @@ console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
 // 6.1 checkPrime
 function checkPrime(num) {
-    // YOUR CODE HERE
+    for (let i = 2; i<= Math.floor(Math.sqrt(num)); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
+console.log(checkPrime(3));
 
 // 6.2 printPrimes
 function printPrimes(num) {
-    // YOUR CODE HERE
+    for (let i = 2; i <= num; i++) {
+        if(checkPrime(i)) console.log(i)
+    }
 }
+console.log(`printPrimes(97)`)
+
 
 // 7. printLongestWord
 function printLongestWord(arr) {
