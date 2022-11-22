6fb10b6d0b802b9b14c6a6dca73a160a116a0f14
vasilismyrianth@gmail.com <vasilis@Vasiliss-MacBook-Pro.local>
2022-11-22 10:03:35 -0500

    Homework challenegd me a bit so little late

--- app.js
+++ app.js
@@ -7,37 +7,70 @@
 // 2. calculateCube
 function calculateCube(num) {
     // YOUR CODE HERE
+    return num * num * num
 }
-
+console.log(calculateCube(5));
 // 3. isAVowel
 function isAVowel(letter) {
     // YOUR CODE HERE
+    letter = ("a","e","i","o","u")
+    return true;
 }
-
+console.log(isAVowel("a"));
 // 4. getTwoLengths
 function getTwoLengths(word1, word2) {
     // YOUR CODE HERE
-}
+    array = [];
+    array.push(word1.length, word2.length)
+return array
 
+}
+console.log(getTwoLengths("Hank", "Hippopopalous"));
 // 5. sumArray
 function sumArray(arr) {
     // YOUR CODE HERE
-}
+    let total = 0
+    for(let i = 0; i < arr.length; i++){
+    total = total + arr[i];}
+    return total
 
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
 // 6.1 checkPrime
 function checkPrime(num) {
     // YOUR CODE HERE
+    for(let i = 2; i <= Math.sqrt(num); i++){
+     if (num % i == 0) {
+        return false;
+     }
+    }
+    return true
 }
+
+//console.log(checkPrime(2))
 
 // 6.2 printPrimes
 function printPrimes(num) {
-    // YOUR CODE HERE
+    for (let i = 2; i <= num; i++) {
+      if (checkPrime(i) === true) {
+        console.log(i)
+      }  
+    }
 }
+
+console.log(printPrimes(97));
 
 // 7. printLongestWord
 function printLongestWord(arr) {
-    // YOUR CODE HERE
+    let word = ""
+    for(let i = 0; i < arr.length; i++) {
+        if (word.length <arr[i].length) {
+            word = arr[i];
+        }
+    }
+    return word;
 }
+console.log(printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "blob"]));
 
 // BONUS!
 
